7f77592a1b59a59510953baa744424f08d9775e5
Matteo <Matteo@Matebook14-Matteo>
2022-12-08 12:23:35 +0100

    initial commit of the project.

---
+++ PrimaProva/src/main/java/test/Main.java
@@ -0,0 +1,28 @@
+package test;
+
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ciao");
+		int x = 4;
+		System.out.println(calcolaQuadro(x));
+	}
+
+
+		
+public static int calcolaQuadro (int x) {
+			
+			int y = x * x;
+			return y;
+}
+
+
+
+}
+
+
