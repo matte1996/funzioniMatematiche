c5bbbd89578499134a2ff28628204c484ce1407e
Matteo <Matteo@Matebook14-Matteo>
2022-12-08 19:02:43 +0100

    funzione area cerchio

--- PrimaProva/src/main/java/test/Main.java
+++ PrimaProva/src/main/java/test/Main.java
@@ -1,21 +1,24 @@
 package test;
 
 
-
-
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 4;
-		System.out.println("il numero inserito vale " + x);
+		int x = 8;
+		System.out.println("Il numero inserito è " + x);
 		System.out.println("ne calcolo il quadrato " + calcolaQuadro(x));
 		System.out.println("ne calcolo la meta  " + calcolaMeta(x));
 		System.out.println("ne calcolo il doppio " + calcolaRaddoppio(x));
-		System.out.println("testxx");
+		System.out.println("L'area del cerchio di raggio " + x + " vale "+ calcolaAreaCerchio(x));
 
 	}
+	
+public static Double calcolaAreaCerchio (double x) {
+		
+		double y = x * x *Math.PI;
+		return y;
+}
 		
 public static int calcolaQuadro (int x) {
 			
